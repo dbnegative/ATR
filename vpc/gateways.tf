c9a06abb1bbf12d4bc7faf5ffd63397d2fdf1e87
dbnegative <shortjay@gmail.com>
2017-03-14 20:09:23 +0000

    removed unnecassery public nayt gateway

--- vpc/gateways.tf
+++ vpc/gateways.tf
@@ -1,8 +1,3 @@
-resource "aws_nat_gateway" "public_gw" {
-  allocation_id = "${element(aws_eip.gateways.*.id, count.index)}"
-  subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
-}
-
 resource "aws_nat_gateway" "private_gw" {
   allocation_id = "${element(aws_eip.gateways.*.id, count.index)}"
   subnet_id     = "${element(aws_subnet.private.*.id, count.index)}"
@@ -10,6 +5,10 @@ resource "aws_nat_gateway" "private_gw" {
 
 resource "aws_internet_gateway" "gateway" {
   vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name = "${var.aws_vpc_name} IGW"
+  }
 }
 
 resource "aws_eip" "gateways" {

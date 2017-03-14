a92f6229cf0f1441403c5cc1eb3bfad1848480df
dbnegative <shortjay@gmail.com>
2017-03-14 19:55:50 +0000

    fixed subnet creation

--- vpc/subnet.tf
+++ vpc/subnet.tf
@@ -2,9 +2,8 @@ resource "aws_subnet" "public" {
   count = "${length(var.aws_azs)}"
 
   vpc_id            = "${aws_vpc.vpc.id}"
-  availability_zone = "${element(var.aws_azs, count.index)}"
+  availability_zone = "${var.aws_region}${element(var.aws_azs, count.index)}"
   cidr_block        = "${cidrsubnet(var.vpc_cidr, 7, count.index+4)}"
-  availability_zone = "${var.aws_region}"
 
   tags {
     Name = "public-${var.aws_region}"
@@ -16,9 +15,8 @@ resource "aws_subnet" "private" {
   count = "${length(var.aws_azs)}"
 
   vpc_id            = "${aws_vpc.vpc.id}"
-  availability_zone = "${element(var.aws_azs, count.index)}"
+  availability_zone = "${var.aws_region}${element(var.aws_azs, count.index)}"
   cidr_block        = "${cidrsubnet(var.vpc_cidr, 7, count.index+1)}"
-  availability_zone = "${var.aws_region}"
 
   tags {
     Name = "private-${var.aws_region}"

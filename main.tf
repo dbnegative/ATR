2a3f65aede515536912cc2a83ab9e5a6d1f0f0a5
dbnegative <shortjay@gmail.com>
2017-03-15 10:29:24 +0000

    added rancher node module

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
 module "vpc" {
-  source       = "./vpc"
-  aws_region   = "${var.aws_region}"
-  aws_vpc_name = "my_terraform_vpc"
-  vpc_cidr     = "10.0.0.0/16"
+  source           = "./vpc"
+  aws_region       = "us-west-1"
+  vpc_name         = "production"
+  vpc_cidr         = "10.0.0.0/8"
+  hosted_zone_name = "example.com"
+  ssh_pubkey       = "./ssh/aws_ssh.pub"
+}
+
+module "node" {
+  source          = "./node"
+  vpc_name        = "production"
+  aws_region      = "us-west-1"
+  cluster_size    = "3"
+  image_id        = "ami-7b09571b"
+  vpc_id          = "${module.vpc.vpc_id}"
+  private_subnets = "${module.vpc.private_subnets}"
 }

c38ceb4cdf0fb4b9688c594d6f6f20c3b7dd157b
dbnegative <shortjay@gmail.com>
2017-03-15 10:27:24 +0000

    added some default vars

--- vars.tf
+++ vars.tf
@@ -2,3 +2,15 @@ variable "aws_region" {
   description = "AWS EC2 Region for VPC"
   default     = "us-east-1"
 }
+
+variable "hosted_zone_name" {
+  type        = "string"
+  description = "Name of hosted zone"
+  default     = "example.com"
+}
+
+variable "ssh_pubkey" {
+  type        = "string"
+  description = "Path to the SSH public key"
+  default     = "./ssh/aws_ssh.pub"
+}

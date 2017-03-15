987e3cabc4768828d65b4faba065697bfbddb101
dbnegative <shortjay@gmail.com>
2017-03-15 10:26:18 +0000

    added vpc dhcp options and keypairs

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,6 +4,25 @@ resource "aws_vpc" "vpc" {
   enable_dns_support   = "${var.vpc_options["enable_dns_support"]}"
 
   tags {
-    Name = "${var.aws_vpc_name}"
+    Name = "${var.vpc_name}"
   }
+}
+
+resource "aws_vpc_dhcp_options" "dns_search_path" {
+  domain_name         = "priv.${var.hosted_zone_name}"
+  domain_name_servers = ["${cidrhost(var.vpc_cidr, 2)}"]
+
+  tags {
+    Name       = "${var.vpc_name} - FQDN search"
+    DomainName = "${var.hosted_zone_name}"
+  }
+}
+
+resource "template_file" "ssh_keypair" {
+  template = "${file(var.ssh_pubkey)}"
+}
+
+resource "aws_key_pair" "ssh_keypair" {
+  key_name   = "${var.vpc_name}"
+  public_key = "${template_file.ssh_keypair.rendered}"
 }

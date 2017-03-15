f38973ab3c178b1b4dcc83998b7432bd17a0b4b0
dbnegative <shortjay@gmail.com>
2017-03-15 10:28:02 +0000

    Add required variables file for node module

---
+++ node/variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_name" {
+  description = "AWS VPC Name"
+}
+
+variable "cluster_size" {
+  description = "Cluster size"
+}
+
+variable "aws_region" {
+  description = "AWS EC2 Region for VPC"
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+}
+
+variable "image_id" {
+  description = "AMI ID"
+}

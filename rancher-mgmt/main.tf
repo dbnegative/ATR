a1a7adbf1ef9406423dad857efa5a5ca451c4b64
dbnegative <shortjay@gmail.com>
2017-04-10 11:36:18 +0100

    Updated comments and removed elb http ingress

--- rancher-mgmt/main.tf
+++ rancher-mgmt/main.tf
@@ -50,6 +50,7 @@ resource "acme_certificate" "certificate" {
   registration_url = "${acme_registration.reg.id}"
 }
 
+# Upload Lets Encrypt cert to AWS
 resource "aws_iam_server_certificate" "rancher_mgmt_cert" {
   name_prefix      = "rancher-mgmt-cert"
   certificate_body = "${acme_certificate.certificate.certificate_pem}"
@@ -60,6 +61,7 @@ resource "aws_iam_server_certificate" "rancher_mgmt_cert" {
   }
 }
 
+# Create elb to load balance between rancher management instances
 resource "aws_elb" "rancher_mgmt_elb" {
   name            = "rancher-mangement-elb"
   subnets         = ["${var.public_subnets}"]
@@ -91,24 +93,11 @@ resource "aws_elb" "rancher_mgmt_elb" {
   }
 }
 
+# Create security group to allow access to elb SSL port
 resource "aws_security_group" "rancher_mgamt_elb_sec_group" {
   name        = "rancher managment elb SG"
   description = "rancher managment elb security group"
   vpc_id      = "${var.vpc_id}"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "TCP"
-    cidr_blocks = ["10.0.0.0/8"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   ingress {
     from_port   = 443
@@ -138,6 +127,7 @@ resource "aws_security_group" "rancher_mgamt_elb_sec_group" {
   }
 }
 
+# Create rancher management instances ASG 
 resource "aws_autoscaling_group" "rancher_mgmt_asg" {
   name = "Rancher mgmt ASG"
 
@@ -178,6 +168,7 @@ resource "aws_autoscaling_group" "rancher_mgmt_asg" {
   }
 }
 
+# Create template file for userdata to register management instances on boot
 data "template_file" "mgmt_userdata" {
   template = "${file("${path.module}/userdata.yml.tpl")}"
 
@@ -190,6 +181,7 @@ data "template_file" "mgmt_userdata" {
   }
 }
 
+# Create rancher management instance launch config with userdata
 resource "aws_launch_configuration" "rancher_mgmt_launch_config" {
   lifecycle {
     create_before_destroy = true
@@ -208,6 +200,7 @@ resource "aws_launch_configuration" "rancher_mgmt_launch_config" {
   user_data = "${data.template_file.mgmt_userdata.rendered}"
 }
 
+# Create SG for management nodes allowing ingress od ssh, cluster registratation, and web interface
 resource "aws_security_group" "rancher_mgmt_sec_group" {
   name        = "rancher-mgmt"
   description = "rancher mgmt security group"
@@ -248,11 +241,13 @@ resource "aws_security_group" "rancher_mgmt_sec_group" {
   }
 }
 
+# Create managment node instance proile
 resource "aws_iam_instance_profile" "rancher_mgmt_iam_profile" {
   name  = "${var.vpc_name}-rancher-mgmt-${var.aws_region}"
   roles = ["${aws_iam_role.rancher_mgmt_iam_role.name}"]
 }
 
+# Create managment node IAM role
 resource "aws_iam_role" "rancher_mgmt_iam_role" {
   name = "${var.vpc_name}-rancher-mgmt-${var.aws_region}"
 
@@ -271,6 +266,7 @@ resource "aws_iam_role" "rancher_mgmt_iam_role" {
 EOF
 }
 
+# Create IAM policy for management nodes
 resource "aws_iam_role_policy" "rancher_mgmt_iam_policy" {
   name = "rancher-mgmt-peers"
   role = "${aws_iam_role.rancher_mgmt_iam_role.id}"
@@ -292,6 +288,7 @@ resource "aws_iam_role_policy" "rancher_mgmt_iam_policy" {
 EOF
 }
 
+# Create RDS subnet group for rancher management mysql backend
 resource "aws_db_subnet_group" "rancher_rds_subnet_group" {
   name       = "rancherdb"
   subnet_ids = ["${var.private_subnets}"]
@@ -301,12 +298,14 @@ resource "aws_db_subnet_group" "rancher_rds_subnet_group" {
   }
 }
 
+# Create base empty SG for allowing rancher management nodes access to mysql 
 resource "aws_security_group" "rancher_rds_allow_mysql" {
   name        = "allow_rancher_mysql"
   description = "Allow all inbound traffic"
   vpc_id      = "${var.vpc_id}"
 }
 
+# Create SG rule for allowing rancher management nodes in all AZ's access to mysql 
 resource "aws_security_group_rule" "allow_mysql" {
   count = "${length(var.aws_azs)}"
 
@@ -318,6 +317,7 @@ resource "aws_security_group_rule" "allow_mysql" {
   security_group_id = "${aws_security_group.rancher_rds_allow_mysql.id}"
 }
 
+# Create rancher mysql backend to store all rancher config
 resource "aws_db_instance" "rancher_rds" {
   allocated_storage      = 10
   storage_type           = "gp2"

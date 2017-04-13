5ad49083db0858556e75768fce0dd1e1293b8dbf
dbnegative <shortjay@gmail.com>
2017-04-13 09:44:41 +0100

    Added cert outputs

--- rancher-mgmt/output.tf
+++ rancher-mgmt/output.tf
@@ -1,3 +1,11 @@
 output "rds_endpoint" {
   value = "${aws_db_instance.rancher_rds.endpoint.address}"
 }
+
+output "elb_cert_pem" {
+  value = "${acme_certificate.certificate.certificate_pem}"
+}
+
+output "elb_private_key_pem" {
+  value = "${acme_certificate.certificate.private_key_pem}"
+}

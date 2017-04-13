577019e7338d1fbc9960fad396735f60287de616
dbnegative <shortjay@gmail.com>
2017-04-13 10:05:44 +0100

    updated exposed variables and set some example default values

--- main.tf
+++ main.tf
@@ -9,19 +9,22 @@ module "vpc" {
 }
 
 module "rancher-mgmt" {
-  source          = "./rancher-mgmt"
-  vpc_name        = "production"
-  aws_region      = "us-west-1"
-  cluster_size    = "2"
-  image_id        = "ami-7b09571b"
-  vpc_id          = "${module.vpc.vpc_id}"
-  vpc_cidr        = "10.0.0.0/16"
-  aws_azs         = ["us-west-1c", "us-west-1b"]
-  private_subnets = "${module.vpc.private_subnets}"
-  public_subnets  = "${module.vpc.public_subnets}"
-  root_dbusername = "rancheradmin"
-  root_dbpassword = "rancheradmin"
-  dbname          = "rancherdb"
+  source               = "./rancher-mgmt"
+  vpc_name             = "production"
+  aws_region           = "us-west-1"
+  cluster_size         = "2"
+  image_id             = "ami-7b09571b"
+  vpc_id               = "${module.vpc.vpc_id}"
+  vpc_cidr             = "10.0.0.0/16"
+  aws_azs              = ["us-west-1c", "us-west-1b"]
+  private_subnets      = "${module.vpc.private_subnets}"
+  public_subnets       = "${module.vpc.public_subnets}"
+  root_dbusername      = "rancheradmin"
+  root_dbpassword      = "rancheradmin"
+  dbname               = "rancherdb"
+  hosted_zone_id       = "Z6Z290XNPS459A"
+  domain               = "example.com"
+  domain_email_address = "you@example.com"
 }
 
 module "bastion" {
@@ -32,4 +35,6 @@ module "bastion" {
   vpc_id          = "${module.vpc.vpc_id}"
   private_subnets = "${module.vpc.private_subnets}"
   public_subnets  = "${module.vpc.public_subnets}"
+  hosted_zone_id  = "Z6Z290XNPS459A"
+  domain          = "example.com"
 }
